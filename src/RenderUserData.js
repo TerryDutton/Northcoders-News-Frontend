47f3b03ea36c1da2a327baf911567e9b26e85e73
TerryDutton <t_dutton85@hotmail.com>
2018-06-24 09:21:50 +0100

    Adding fontAwesome fonts

--- src/RenderUserData.js
+++ src/RenderUserData.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom'; 
+import blankAvatar from './views/blankAvatar';
 
 function RenderUserData({user}){
   const {username, name, avatar_url, _id} = user;
@@ -8,7 +9,7 @@ function RenderUserData({user}){
       <li key={`${_id}li`}>
         <h1><Link to={`/api/users/${username}`}>{username}</Link></h1>
         <h6>{name || ''}</h6>
-        <img src={avatar_url} alt={`${username}'s avatar`}/>
+        <img src={avatar_url} className="avatarLg" alt={`${username}'s avatar`} onError={e => e.target.src = blankAvatar}/>
       </li>
     </ul>
   );
