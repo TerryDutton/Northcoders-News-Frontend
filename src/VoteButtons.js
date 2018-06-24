a74def1a616682b46d0d650255296d55057f8448
TerryDutton <t_dutton85@hotmail.com>
2018-06-24 10:16:49 +0100

    Adding fontAwesome fonts

--- src/VoteButtons.js
+++ src/VoteButtons.js
@@ -3,8 +3,8 @@ import React from 'react';
 function VoteButtons({voteFunction, targetID}){
   return (
     <React.Fragment>
-      <input type="button" className={'button'} title={"Vote up"}  value={"🢁"} onClick={() => voteFunction(targetID, 1)}/>
-      <input type="button" className={'button'} title={"Vote down"}value={"🢃"} onClick={() => voteFunction(targetID, -1)}/>
+      <input type="button" className={'button fas fa-arrow-up'} title={"Vote up"}  value={'\uf062'} onClick={() => voteFunction(targetID, 1)}/>
+      <input type="button" className={'button fas fa-arrow-down'} title={"Vote down"}value={"\uf063"} onClick={() => voteFunction(targetID, -1)}/>
     </React.Fragment>
   );
 }
